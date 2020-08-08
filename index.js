80d6f687e5c9266dca7bc13f8e60f38972b74590
Saeel Parulekar <saeelparulekar>
2020-08-09 01:58:13 +0530

    added Cors update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    // res.setHeader('Access-Control-Allow-Origin', 'https://shopifyeventlogs.web.app');
+    res.setHeader('Access-Control-Allow-Origin', 'https://shopifyeventlogs.web.app');
 
 
     // Request methods you wish to allow
