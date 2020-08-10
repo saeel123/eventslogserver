3bcff52d43030bfae206860d4518b11dee93a7d3
Saeel Parulekar <saeelparulekar>
2020-08-10 15:27:07 +0530

    added get route updated

--- routers/event.js
+++ routers/event.js
@@ -6,7 +6,10 @@ const axios = require('axios');
 
 const axiosInstance = axios.create({
     baseURL: 'https://full-stack-dev-test.myshopify.com/admin/api/2020-01',
-    headers: { 'X-Shopify-Access-Token': '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', 'Content-Type': 'application/json' },
+    headers: {
+        'X-Shopify-Access-Token': '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',
+        'Content-Type': 'application/json'
+    },
 });
 
 const saveEvents = function (results) {
@@ -30,92 +33,102 @@ let originalRes = null
 const checkNextUrl = function (response) {
     if (response.headers.link && response.headers.link.indexOf(`rel="next"`) > -1) {
         try {
-          let nextLink = response.headers.link;
-          if (nextLink.indexOf(`rel="previous"`) > -1) {
-            nextLink = nextLink.substr(nextLink.indexOf(",") + 2, nextLink.length);
-          } 
-          nextLink = nextLink.substr(1, nextLink.indexOf(">") - 1);
-          callNextEventsPage(nextLink)
-            
-        } catch(ex) {
-            console.log( 'Failed to parse nextlink');
-            originalRes.status(201).send({message: 'Error occured on syn', success: false});
-            console.log(response.headers)
+            let nextLink = response.headers.link;
+            if (nextLink.indexOf(`rel="previous"`) > -1) {
+                nextLink = nextLink.substr(nextLink.indexOf(",") + 2, nextLink.length);
+            }
+            nextLink = nextLink.substr(1, nextLink.indexOf(">") - 1);
+            callNextEventsPage(nextLink)
+        } catch (ex) {
+            console.log('Failed to parse nextlink');
+            originalRes.status(201).send({
+                message: 'Error occured on syn',
+                success: false
+            });
         }
     } else {
-        originalRes.status(201).send({message: 'Events Synced', success: true});
+        originalRes.status(201).send({
+            message: 'Events Synced',
+            success: true
+        });
     }
 }
 
 const callNextEventsPage = function (url) {
-    axios( {
+    axios({
             method: 'get',
             url: url,
-            headers: { 'X-Shopify-Access-Token': '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', 'Content-Type': 'application/json' }
-    })
-    .then(function (response) {
-        saveEvents(response.data);
-        checkNextUrl(response);            
-    })
-    .catch(function (error) {
-        console.log(error);
-    })
-    .finally(function () {
-        // always executed
-    }); 
+            headers: {
+                'X-Shopify-Access-Token': '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',
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(function (response) {
+            saveEvents(response.data);
+            checkNextUrl(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+        .finally(function () {
+            // always executed
+        });
 }
 
-
-
-
+//Sync API
 router.get('/syncevent', async (req, res) => {
     originalRes = res;
-    
+
     try {
 
         const getLocalEventCount = new Promise((resolve, reject) => {
-            Event.count({}, function( err, count){
-            resolve(count);
+            Event.count({}, function (err, count) {
+                resolve(count);
             });
         });
-        
+
         const getRemoteEventCount = new Promise((resolve, reject) => {
-            axiosInstance.get('/events/count.json').then( response => {
+            axiosInstance.get('/events/count.json').then(response => {
                 resolve(response.data.count);
             }).catch(error => {
                 return reject('error occured while fetching records');
             });
         });
-        
+
         const getFirstLocalLatestEvent = new Promise((resolve, reject) => {
             const event = Event.findOne({})
-            .sort({'event_id' : -1 });
+                .sort({
+                    'event_id': -1
+                });
             resolve(event);
         });
 
         let localDbEventsCount = await getLocalEventCount;
-        console.log(localDbEventsCount);
-        
+
         if (localDbEventsCount === 0) {
             const response = await axiosInstance.get('/events.json?limit=250');
             saveEvents(response.data);
-            checkNextUrl(response, res);  
+            checkNextUrl(response, res);
 
         } else {
             let remoteDbEventsCount = await getRemoteEventCount;
-            console.log(remoteDbEventsCount);
             if (localDbEventsCount === remoteDbEventsCount) {
-                res.status(201).send({message: 'Events Synced', success: true})
+                res.status(201).send({
+                    message: 'Events Synced',
+                    success: true
+                })
             } else {
-                console.log("get first record Dump");
                 const event = await getFirstLocalLatestEvent
-                const response = await axiosInstance.get('/events.json?limit=250&since_id='+event.event_id);
+                const response = await axiosInstance.get('/events.json?limit=250&since_id=' + event.event_id);
 
                 if (response.data.events.length === 0) {
-                    res.status(201).send({message: 'Events Synced', success: true});
+                    res.status(201).send({
+                        message: 'Events Synced',
+                        success: true
+                    });
                 } else {
                     saveEvents(response.data);
-                    checkNextUrl(response, res);          
+                    checkNextUrl(response, res);
                 };
             }
         }
@@ -124,5 +137,86 @@ router.get('/syncevent', async (req, res) => {
         console.error(error);
     }
 });
+
+
+//get events
+router.get('/event', async (req, res) => {
+    const resPerPage = req.query.limit;
+    const page = req.query.page || 1;
+    const sort = {}
+    const where_conditions = {}
+
+
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    }
+
+    //date filter
+    if ((req.query.startDate) && (req.query.endDate)) {
+        where_conditions.event_created_at = {
+            $lt: new Date(req.query.endDate * 1000).toISOString(),
+            $gte: new Date(req.query.startDate * 1000).toISOString()
+        }
+    }
+    if ((req.query.startDate) && (!req.query.endDate)) {
+        where_conditions.event_created_at = {
+            $gte: new Date(req.query.startDate * 1000).toISOString()
+        }
+    }
+    if ((!req.query.startDate) && (req.query.endDate)) {
+        where_conditions.event_created_at = {
+            $lt: new Date(req.query.endDate * 1000).toISOString()
+        }
+    }
+    //verb filter
+    if (req.query.verb) {
+        if (req.query.verb != "all") {
+            where_conditions.verb = {
+                $regex: new RegExp('.*' + req.query.verb.toString().trim(), "i")
+            }
+        }
+    }
+    //message filter
+    if (req.query.message) {
+        if (req.query.message != "all") {
+            where_conditions.message = {
+                $regex: new RegExp('.*' + req.query.message.toString().trim(), "i")
+            }
+        }
+    }
+    //author filter
+    if (req.query.author) {
+        if (req.query.author != "all") {
+            where_conditions.author = {
+                $regex: new RegExp('.*' + req.query.author.toString().trim(), "i")
+            }
+        }
+    }
+
+    try {
+        const events = await Event.find(where_conditions)
+            .skip(parseInt((resPerPage * page) - resPerPage))
+            .limit(parseInt(resPerPage))
+            .sort(sort)
+
+        const count = await Event.count(where_conditions)
+        const total_count = await Event.count({})
+        const response = {
+            data: events,
+            count: count,
+            total_count: total_count
+        }
+
+        if (!events) {
+            return res.status(404).send();
+        }
+
+        res.status(200).send(response);
+    } catch (e) {
+        res.status(500).send();
+    }
+});
+
 
 module.exports = router
