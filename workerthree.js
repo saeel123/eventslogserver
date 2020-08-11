1622574d0af309970c44855455edb3bdcaa32d9f
Saeel Parulekar <saeelparulekar>
2020-08-12 01:06:46 +0530

    save events updated sync

--- workerthree.js
+++ workerthree.js
@@ -25,8 +25,8 @@ const saveEvents = function (results) {
                 verb: event.verb
             }
             
-            // const newEvent = new Event(eventObj)
-            // newEvent.save()
+            const newEvent = new Event(eventObj)
+            newEvent.save()
         });
     }
     
